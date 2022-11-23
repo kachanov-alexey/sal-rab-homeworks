6039d39812d20eaa1d0033e476a3d004dc754ede
KachanovAlexey <kachanov.alexey@mail.ru>
2022-11-24 00:08:16 +0500

    tast_final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,20 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {goods: [], order: {}};  //client, client: "", 
 
     let countOfGoods = goods.length;
+    
+    data.client = `${name} ${phone}`;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    data.order.sum = sum;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i++) {
+        data.goods.push({title:goods[i].title,count:goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    let jsonData = JSON.stringify({data:data});
 
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+    console.log(jsonData);
     return jsonData;
 }
